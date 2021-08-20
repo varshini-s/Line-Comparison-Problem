442659b0171c8b5d7c1e9f897e036cd5f2e88d0d
varshini <dreamvarshini@gmail.com>
2021-08-20 16:01:06 +0530

    ADDED:initial code in master branch

---
+++ src/com/bridgelabz/Linecomparision/LineComparision.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.Linecomparision;
+
+public class LineComparision {
+	
+	public static void main(String args[]) {
+		
+		System.out.println("----------welcome to line comparision Computation problem----------");
+	}
+
+}
